4c16eb2283770f9b1a51eff7451347a908ab68c7
durban.zhang <durban.zhang@gmail.com>
2017-05-13 23:07:40 +0800

    initial commit

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    let cardNames = ["ace","card2","card3","card4","card5","card6","card7","card8","card9","card10","jack","queen","king"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +28,17 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func dealTapped(_ sender: Any) {
+        let currentDate = NSDate()
+        
+        let leftNumber:Int = Int(arc4random_uniform(13))
+        print("leftNumber \(leftNumber)", currentDate)
+        leftImageView.image = UIImage(named: cardNames[leftNumber])
+        
+        
+        let rightNumber:Int = Int(arc4random_uniform(9))
+        print("rightNumber \(rightNumber)", currentDate)
+        rightImageView.image = UIImage(named: cardNames[rightNumber])
+    }
 }
 
